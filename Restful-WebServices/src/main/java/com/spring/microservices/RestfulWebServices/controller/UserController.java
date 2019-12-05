771e9c421562d01d95ee468b9e68572288318722
sjalihal <sjalihal@SJALIHAL-2M4YV.partnet.cisco.com>
2019-12-05 14:42:54 +0530

    Post used to create a resource and used SOAPUI to test it.

--- Restful-WebServices/src/main/java/com/spring/microservices/RestfulWebServices/controller/UserController.java
+++ Restful-WebServices/src/main/java/com/spring/microservices/RestfulWebServices/controller/UserController.java
@@ -1,16 +1,18 @@
 package com.spring.microservices.RestfulWebServices.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.spring.microservices.RestfulWebServices.HelloWorldBean;
 import com.spring.microservices.RestfulWebServices.dao.DaoService;
 import com.spring.microservices.RestfulWebServices.model.User;
 
@@ -34,9 +36,10 @@ public class UserController {
 	}
 	
 	@PostMapping("/users/create")
-	public void  createUser(@RequestBody User user) {
+	public ResponseEntity<Object>  createUser(@RequestBody User user) {
 		User userReturn = service.save(user);
-		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{userID}").buildAndExpand(userReturn.getId()).toUri();
+		return ResponseEntity.created(location).build();
 	}
 	
 	
